86fcce40e8de4569fedf3c5a7aee23ea917ab7d6
Anthony Mitchell <j.ant.mitch@gmail.com>
2018-05-16 21:57:34 -0400

    update to server file

--- server.js
+++ server.js
@@ -2,3 +2,24 @@ var express = require("express");
 
 var bodyParser = require("body-parser");
 
+var exphbs = require("express-handlebars");
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/tacos_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function(){
+    console.log("App now listening at localhost: " + PORT);
+});
