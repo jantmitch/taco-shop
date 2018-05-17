cfd4e67d5abd4bf8a7fd92ea24902de8763fb9d9
Anthony Mitchell <j.ant.mitch@gmail.com>
2018-05-16 22:00:52 -0400

    update to server JS

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var exphbs = require("express-handlebars");
 
 var PORT = process.env.PORT || 8080;
 
+var app = express();
+
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true}));
